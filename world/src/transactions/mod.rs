0908b63d274eebcc11a754b4ba620b3cacc82692
Agustin Chiappe Berrini <jnieve@gmail.com>
2019-09-26 06:07:23 -0700

    change how we serialize

--- world/src/transactions/mod.rs
+++ world/src/transactions/mod.rs
@@ -6,7 +6,7 @@ use serde::ser::SerializeStruct;
 pub mod pool;
 
 /// Core data structure for interacting with the EVM
-#[derive(Debug, Default, Clone, PartialEq)]
+#[derive(Debug, Default, Deserialize, Clone, PartialEq, Serialize)]
 pub struct Transaction {
     /// Nonce
     pub nonce: U256,
@@ -27,29 +27,6 @@ pub struct Transaction {
     pub r: U256,
     /// The S field of the signature.
     pub s: U256,
-}
-
-impl Serialize for Transaction {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut state = serializer.serialize_struct("Transaction", 9)?;
-        state.serialize_field("nonce", &self.nonce.as_u64())?;
-        state.serialize_field("gasPrice", &self.gas_price.as_u64())?;
-        state.serialize_field("startGas", &self.start_gas.as_u64())?;
-        if self.to.is_none() {
-            state.serialize_field("to", &0)?;
-        } else {
-            state.serialize_field("to", &self.value.as_u64())?;
-        }
-        state.serialize_field("value", &self.value.as_u64())?;
-        state.serialize_field("data", &self.data)?;
-        state.serialize_field("v", &self.v.as_u64())?;
-        state.serialize_field("r", &self.r.as_u64())?;
-        state.serialize_field("s", &self.s.as_u64())?;
-        state.end()
-    }
 }
 
 /// A valid transaction is one where:
